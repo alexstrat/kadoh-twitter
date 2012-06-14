0e16837d81cc529e16c641382507978188a6da52
Alexandre Lacheze <alexandre.lacheze@gmail.com>
2012-06-14 16:49:03 +0200

    nothing to do there

--- app/views/test.js
+++
@@ -1,9 +0,0 @@
-var text = require('fs').readFileSync(__dirname+'/tweet.jade');
-
-fn = require('jade').compile(text);
-
-console.log(fn({
-  author : 'alex',
-  text : 'fofofoofo',
-  date : 'tody',
-  hashtags : ['foo', 'bar']}));
