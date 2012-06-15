bbc9097f86b8a561cf14b358dd31b5e94321b29f
Alexandre Lacheze <alexandre.lacheze@gmail.com>
2012-06-15 16:56:50 +0200

    bad things0

--- app/controler/router.js
+++ app/controler/router.js
@@ -1,10 +1,39 @@
 var Backbone = require('backbone-browserify');
+var $ = require('jquery-browserify');
+var Connection = require('../views/connection');
+var Collection = require('../models/tweet-collection');
+var TimeLine = require('../views/timeline')
 
 module.exports = Backbone.Router.extend({
+
+  initialize: function() {
+    this.connectionView = new connectionView();
+    $('body').append(this.connectionView.render().$el);
+
+    this.connectionView.on('connection', this.connect, this);
+  },
+
+
   routes: {
     'user/:user' : 'navigateToUser',
     'time/:time' : 'navigateToTime',
     'hashtag/:hashtag' : 'navigateToHashTag',
   }
 
+  connect: function(cred) {
+    //todo connection
+    //set this.twitterNode();
+    //this.navigate('time/'+Date.now());
+  },
+
+  loadTimelineView: function(url) {
+    if(this.timelineView)
+      this.timelineView.remove();
+
+    var tweets = new Collection({
+      url : url,
+      twitterNode : this.twitterNode});
+    var timeline = new TimeLine(tweets);
+    tweets.fetch();
+  }
 })
