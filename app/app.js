8ed5b7309ef202d58381dc332de86a4c5289e12c
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-06-24 16:55:46 +0200

    No error handling

--- app/app.js
+++ app/app.js
@@ -54,13 +54,8 @@ function cubeProxy(options) {
       var buf = '';
       req.on('data', function(chunk) { buf += chunk; });
       req.on('end', function() {
-        socket.send(new Buffer(buf), 0, buf.length, port, host, function(err) {
-          if (err) {
-            res.end(500);
-          } else {
-            res.end(200);
-          }
-        });
+        socket.send(new Buffer(buf), 0, buf.length, port, host);
+        res.end();
       });
     } else {
       next();
